ef7a792ce9a437acf74c4a8d517d232efac931ad
escttsalo <escttsalo@gmail.com>
2021-07-22 04:55:48 -0600

    is even number

--- index.js
+++ index.js
@@ -176,8 +176,11 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
-  // ✨ implement
+async function isEvenNumberAsync(number) {
+  if (typeof number != 'number' || isNaN(number)){
+    throw new Error('number must be a number')
+  }
+  return number % 2 === 0 || false
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -140,6 +140,26 @@ describe('[Exercise 6] Car', () => {
 })
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  // test('[20] resolves false if passed an odd number', () => {})
+  test('[19] resolves true if passed an even number', async () => {
+    const res = await utils.isEvenNumberAsync(2)
+    expect(res).toBe(true)
+  })
+  test('[20] resolves false if passed an odd number', async () => {
+    const res = await utils.isEvenNumberAsync(1)
+    expect(res).toBe(false)
+  })
+  test('[21] rejects an error with the message "number must be a number" if passed a non-number type', async () => {
+    try {
+      await utils.isEvenNumberAsync('foobar')
+    } catch (err){
+      expect(err.message).toMatch(/number must be a number/i)
+    }
+  })
+  test('[22] rejects an error with the message "number must be a number" if passed NaN', async () => {
+    try{
+      await utils.isEvenNumberAsync(NaN)
+    } catch (err) {
+      expect(err.message).toMatch(/number must be a number/i)
+    }
+  })
 })
